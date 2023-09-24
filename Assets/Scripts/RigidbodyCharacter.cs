03d09619d6f44336472098267c7de42fdca3fe8b
HDRP <hdrp0720@gmail.com>
2023-09-24 19:26:18 +0900

    add logic of character move by rigidbody

---
+++ Assets/Scripts/RigidbodyCharacter.cs
@@ -0,0 +1,71 @@
+using System;
+using UnityEngine;
+
+public class RigidbodyCharacter : MonoBehaviour
+{
+  #region Variables
+  public float speed = 5f;
+  public float jumpHeight = 2f;
+  public float dashDistance = 5f;
+  public LayerMask groundLayerMask;
+  public float groundCheckDistance = 0.3f;
+
+  private Rigidbody rb;
+  private Vector3 inputDirection = Vector3.zero;
+  private bool bIsGround = false;
+  #endregion
+
+  private void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+  private void Update()
+  {
+    CheckGround();
+      
+    // Implement character movement by user input
+    inputDirection = Vector3.zero;
+    inputDirection.x = Input.GetAxis("Horizontal");
+    inputDirection.z = Input.GetAxis("Vertical");
+    if (inputDirection != Vector3.zero)
+      transform.forward = inputDirection;
+    
+    // Implement character jump by user input
+    if (Input.GetButtonDown("Jump") && bIsGround)
+    {
+      Vector3 jumpVelocity = Vector3.up * Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y);
+      rb.AddForce(jumpVelocity, ForceMode.VelocityChange);
+    }
+    
+    // Implement character dash by user input
+    if (Input.GetButtonDown("Dash"))
+    {
+      Vector3 dashVelocity = Vector3.Scale(transform.forward, 
+        dashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * rb.drag + 1)) / -Time.deltaTime),
+        0,
+        (Mathf.Log(1f / (Time.deltaTime * rb.drag + 1)) / -Time.deltaTime)));
+      rb.AddForce(dashVelocity, ForceMode.VelocityChange);
+    }
+  }
+  private void FixedUpdate()
+  {
+    rb.MovePosition(rb.position + inputDirection * (speed * Time.fixedDeltaTime));
+  }
+
+  private void CheckGround()
+  {
+    RaycastHit hitInfo;
+    Vector3 rayOrigin = transform.position + (Vector3.up * 0.1f);
+    if (Physics.Raycast(rayOrigin,Vector3.down, out hitInfo, groundCheckDistance, groundLayerMask))
+    {
+      bIsGround = true;
+    }
+    else
+    {
+      bIsGround = false;
+    }
+    #if UNITY_EDITOR
+    Debug.DrawLine(rayOrigin, rayOrigin + Vector3.down * groundCheckDistance, Color.red);
+    #endif
+  }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Unity RPG Tutorial
+패스트 캠퍼스 액션 RPG 클론입니다.
+* Unity Version: 2021.3.30f1
+* IDE Rider를 위한 RiderFlow 에셋 사용
+
+## 캐릭터 이동 구현
+* Rigidbody를 이용한 구현
+* Character Controller를 이용한 구현
+* Navmesh agent를 이용한 구현
